563fa7d9f9c3cec63a8e8178f3c09fc3249abe75
hbb <hbb@hbbtekademy.com>
2022-08-25 18:20:35 +0530

    add public methods similar to log package

--- logger.go
+++ logger.go
@@ -12,12 +12,29 @@ type logLevel int
 
 const (
 	_ logLevel = iota
-	DEBUG
-	INFO
-	ERROR
-	FATAL
-	PANIC
+	_DEBUG
+	_INFO
+	_ERROR
+	_FATAL
+	_PANIC
 )
+
+func (ll logLevel) String() string {
+	switch ll {
+	case _DEBUG:
+		return "DEBUG"
+	case _INFO:
+		return "INFO"
+	case _ERROR:
+		return "ERROR"
+	case _FATAL:
+		return "FATAL"
+	case _PANIC:
+		return "PANIC"
+	default:
+		return ""
+	}
+}
 
 var (
 	once     sync.Once
@@ -25,135 +42,177 @@ var (
 	instance logger
 )
 
+func Debug(v ...interface{}) {
+	getInstance().debug(v...)
+}
+
+func Debugf(format string, v ...interface{}) {
+	getInstance().debugf(format, v...)
+}
+
+func Info(v ...interface{}) {
+	getInstance().info(v...)
+}
+
+func Infof(format string, v ...interface{}) {
+	getInstance().infof(format, v...)
+}
+
+func Error(v ...interface{}) {
+	getInstance().error(v...)
+}
+
+func Errorf(format string, v ...interface{}) {
+	getInstance().errorf(format, v...)
+}
+
+func Fatal(v ...interface{}) {
+	getInstance().fatal(v...)
+}
+
+func Fatalf(format string, v ...interface{}) {
+	getInstance().fatalf(format, v...)
+}
+
+func Panic(v ...interface{}) {
+	getInstance().panic(v...)
+}
+
+func Panicf(format string, v ...interface{}) {
+	getInstance().panicf(format, v...)
+}
+
 type logger interface {
-	Infof(format string, v ...interface{})
-	Info(v ...interface{})
-	Debug(v ...interface{})
-	Debugf(format string, v ...interface{})
-	Error(v ...interface{})
-	Errorf(format string, v ...interface{})
-	Fatal(v ...interface{})
-	Fatalf(format string, v ...interface{})
-	Panic(v ...interface{})
-	Panicf(format string, v ...interface{})
+	infof(format string, v ...interface{})
+	info(v ...interface{})
+	debug(v ...interface{})
+	debugf(format string, v ...interface{})
+	error(v ...interface{})
+	errorf(format string, v ...interface{})
+	fatal(v ...interface{})
+	fatalf(format string, v ...interface{})
+	panic(v ...interface{})
+	panicf(format string, v ...interface{})
 }
 
 type stdLogger struct {
 	logLevel    logLevel
-	infoLogger  *log.Logger
 	debugLogger *log.Logger
+	infoLogger  *log.Logger
 	errLogger   *log.Logger
 	fatalLogger *log.Logger
 	panicLogger *log.Logger
 }
 
-func (l stdLogger) Debug(v ...interface{}) {
-	if l.logLevel <= DEBUG {
-		l.debugLogger.Output(2, fmt.Sprintln(v...))
+func (l stdLogger) debug(v ...interface{}) {
+	if l.logLevel <= _DEBUG {
+		l.debugLogger.Output(3, fmt.Sprintln(v...))
 	}
 }
 
-func (l stdLogger) Debugf(format string, v ...interface{}) {
-	if l.logLevel <= DEBUG {
-		l.debugLogger.Output(2, fmt.Sprintf(format, v...))
+func (l stdLogger) debugf(format string, v ...interface{}) {
+	if l.logLevel <= _DEBUG {
+		l.debugLogger.Output(3, fmt.Sprintf(format, v...))
 	}
 }
 
-func (l stdLogger) Info(v ...interface{}) {
-	if l.logLevel <= INFO {
-		l.infoLogger.Output(2, fmt.Sprintln(v...))
+func (l stdLogger) info(v ...interface{}) {
+	if l.logLevel <= _INFO {
+		l.infoLogger.Output(3, fmt.Sprintln(v...))
 	}
 }
 
-func (l stdLogger) Infof(format string, v ...interface{}) {
-	if l.logLevel <= INFO {
-		l.infoLogger.Output(2, fmt.Sprintf(format, v...))
+func (l stdLogger) infof(format string, v ...interface{}) {
+	if l.logLevel <= _INFO {
+		l.infoLogger.Output(3, fmt.Sprintf(format, v...))
 	}
 }
 
-func (l *stdLogger) Error(v ...interface{}) {
-	if l.logLevel <= ERROR {
-		l.errLogger.Output(2, fmt.Sprintln(v...))
+func (l *stdLogger) error(v ...interface{}) {
+	if l.logLevel <= _ERROR {
+		l.errLogger.Output(3, fmt.Sprintln(v...))
 	}
 }
 
-func (l stdLogger) Errorf(format string, v ...interface{}) {
-	if l.logLevel <= ERROR {
-		l.errLogger.Output(2, fmt.Sprintf(format, v...))
+func (l stdLogger) errorf(format string, v ...interface{}) {
+	if l.logLevel <= _ERROR {
+		l.errLogger.Output(3, fmt.Sprintf(format, v...))
 	}
 }
 
-func (l *stdLogger) Fatal(v ...interface{}) {
-	if l.logLevel <= FATAL {
-		l.fatalLogger.Output(2, fmt.Sprintln(v...))
-	}
-	os.Exit(1)
-}
-
-func (l stdLogger) Fatalf(format string, v ...interface{}) {
-	if l.logLevel <= FATAL {
-		l.fatalLogger.Output(2, fmt.Sprintf(format, v...))
+func (l *stdLogger) fatal(v ...interface{}) {
+	if l.logLevel <= _FATAL {
+		l.fatalLogger.Output(3, fmt.Sprintln(v...))
 	}
 	os.Exit(1)
 }
 
-func (l *stdLogger) Panic(v ...interface{}) {
-	if l.logLevel <= PANIC {
+func (l stdLogger) fatalf(format string, v ...interface{}) {
+	if l.logLevel <= _FATAL {
+		l.fatalLogger.Output(3, fmt.Sprintf(format, v...))
+	}
+	os.Exit(1)
+}
+
+func (l *stdLogger) panic(v ...interface{}) {
+	if l.logLevel <= _PANIC {
 		s := fmt.Sprintln(v...)
-		l.panicLogger.Output(2, s)
+		l.panicLogger.Output(3, s)
 		panic(s)
 	}
 }
 
-func (l stdLogger) Panicf(format string, v ...interface{}) {
-	if l.logLevel <= PANIC {
+func (l stdLogger) panicf(format string, v ...interface{}) {
+	if l.logLevel <= _PANIC {
 		s := fmt.Sprintf(format, v...)
-		l.panicLogger.Output(2, s)
+		l.panicLogger.Output(3, s)
 		panic(s)
 	}
 }
 
-func New() logger {
+func getInstance() logger {
 	once.Do(func() {
 		logLevel := getLogLevel()
 		instance = &stdLogger{
 			logLevel:    logLevel,
-			debugLogger: log.New(os.Stdout, "DEBUG: ", log.Lshortfile|log.LstdFlags),
-			infoLogger:  log.New(os.Stdout, "INFO: ", log.Lshortfile|log.LstdFlags),
-			errLogger:   log.New(os.Stderr, "ERROR: ", log.Lshortfile|log.LstdFlags),
-			fatalLogger: log.New(os.Stderr, "FATAL: ", log.Lshortfile|log.LstdFlags),
-			panicLogger: log.New(os.Stderr, "PANIC: ", log.Lshortfile|log.LstdFlags),
+			debugLogger: log.New(os.Stdout, "DEBUG ", log.Lshortfile|log.LstdFlags),
+			infoLogger:  log.New(os.Stdout, "INFO  ", log.Lshortfile|log.LstdFlags),
+			errLogger:   log.New(os.Stderr, "ERROR ", log.Lshortfile|log.LstdFlags),
+			fatalLogger: log.New(os.Stderr, "FATAL ", log.Lshortfile|log.LstdFlags),
+			panicLogger: log.New(os.Stderr, "PANIC ", log.Lshortfile|log.LstdFlags),
 		}
-		instance.Infof("Log Level set to %s", logLevel)
 	})
 
 	return instance
 }
 
 func SetLogLevelEnvVar(envVar string) {
-	if instance == nil {
-		fmt.Fprintf(os.Stderr, "Logger already created.\n")
-		fmt.Fprintf(os.Stderr, "Log Level Env Variable %s should be set before creating an instance of the logger", envVar)
+	if instance != nil {
+		instance.error("Logger already created")
+		instance.errorf("Log Level Env Variable %s should be set before creating an instance of the logger", envVar)
+		return
 	}
 	llEnvVar = envVar
 }
 
 func getLogLevel() logLevel {
-	fmt.Fprintf(os.Stdout, "Getting Log Level from %s", llEnvVar)
+	fmt.Fprintf(os.Stdout, "Getting Log Level from %s env var. Call logger.SetLogLevelEnvVar to override this value\n", llEnvVar)
 	level := strings.TrimSpace(os.Getenv(llEnvVar))
+	ll := _DEBUG
 	if level != "" {
 		switch strings.ToLower(level) {
 		case "debug":
-			return DEBUG
+			ll = _DEBUG
 		case "info":
-			return INFO
+			ll = _INFO
 		case "error":
-			return ERROR
+			ll = _ERROR
 		case "fatal":
-			return FATAL
+			ll = _FATAL
 		case "panic":
-			return PANIC
+			ll = _PANIC
 		}
 	}
-	return DEBUG
+	fmt.Fprintf(os.Stdout, "Log Level set to %s\n", ll)
+	return ll
 }
